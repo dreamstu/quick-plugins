b803b07e7f19c72ce981105f8028bfb0ee3211d4
dreamstu <cjxsgn@163.com>
2015-05-05 20:12:57 +0800

    build

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ quick.util = require('./lib/util').register(quick);
 quick.verbose = quick.log.verbose;
 quick.package = require('./package.json');
 quick.version = quick.package.version;
+quick.require = function(){
+    var path;
+    var name = Array.prototype.slice.call(arguments, 0).join('-');
+    if(quick.require._cache.hasOwnProperty(name)) return quick.require._cache[name];
+    var names = [];
+    for(var i = 0, len = quick.require.prefixes.length; i < len; i++){
+        try {
+            var pluginName = quick.require.prefixes[i] + '-' + name;
+            names.push(pluginName);
+            path = require.resolve(pluginName);
+            try {
+                return quick.require._cache[name] = require(pluginName);
+            } catch (e){
+                quick.log.error('load plugin [' + pluginName + '] error : ' + e.message);
+            }
+        } catch (e){}
+    }
+    quick.log.error('unable to load plugin [' + names.join('] or [') + ']');
+};
+quick.require.prefixes = ['quick'];
+quick.require._cache = {};
